43892bbb8f5facdd805f6ea28335282e1a241312
avn <alexander.nikulin@amtech.ru>
2024-10-15 14:57:29 +0300

    MOSTECHOS-61: change to slf4j

--- src/main/java/ru/mos/mostech/ews/MosTechEws.java
+++ src/main/java/ru/mos/mostech/ews/MosTechEws.java
@@ -8,11 +8,7 @@ import ru.mos.mostech.ews.caldav.CaldavServer;
 import ru.mos.mostech.ews.exception.MosTechEwsException;
 import ru.mos.mostech.ews.exchange.ExchangeSessionFactory;
 import ru.mos.mostech.ews.exchange.auth.ExchangeAuthenticator;
-import ru.mos.mostech.ews.http.HttpClientAdapter;
-import ru.mos.mostech.ews.http.request.GetRequest;
 import ru.mos.mostech.ews.imap.ImapServer;
-import ru.mos.mostech.ews.ldap.LdapServer;
-import ru.mos.mostech.ews.pop.PopServer;
 import ru.mos.mostech.ews.smtp.SmtpServer;
 import ru.mos.mostech.ews.ui.SimpleUi;
 import ru.mos.mostech.ews.ui.tray.MosTechEwsTray;
@@ -27,8 +23,6 @@ import java.util.ArrayList;
  */
 public final class MosTechEws {
     private static final Logger LOGGER = Logger.getLogger(MosTechEws.class);
-    private static final String HTTP_MT_EWS_SOURCEFORGE_NET_VERSION_TXT = "";
-
     private static final Object LOCK = new Object();
     private static boolean shutdown = false;
 
@@ -67,10 +61,10 @@ public final class MosTechEws {
                         .getDeclaredConstructor().newInstance();
                 authenticator.setUsername("");
                 authenticator.authenticate();
-                System.out.println(authenticator.getToken().getRefreshToken());
+                LOGGER.debug(authenticator.getToken().getRefreshToken());
             } catch (IOException | ClassNotFoundException | NoSuchMethodException | InstantiationException |
                      IllegalAccessException | InvocationTargetException e) {
-                System.err.println(e+" "+e.getMessage());
+                LOGGER.error(e + " " + e.getMessage());
             }
             // force shutdown on Linux
             System.exit(0);
@@ -85,7 +79,6 @@ public final class MosTechEws {
                 Settings.setProperty("mt.ews.server", "true");
                 Settings.updateLoggingConfig();
             }
-
 
             if (Settings.getBooleanProperty("mt.ews.server")) {
                 LOGGER.debug("Start MT-EWS in server mode");
@@ -137,10 +130,6 @@ public final class MosTechEws {
         if (smtpPort != 0) {
             SERVER_LIST.add(new SmtpServer(smtpPort));
         }
-        int popPort = Settings.getIntProperty("mt.ews.popPort");
-        if (popPort != 0) {
-            SERVER_LIST.add(new PopServer(popPort));
-        }
         int imapPort = Settings.getIntProperty("mt.ews.imapPort");
         if (imapPort != 0) {
             SERVER_LIST.add(new ImapServer(imapPort));
@@ -148,10 +137,6 @@ public final class MosTechEws {
         int caldavPort = Settings.getIntProperty("mt.ews.caldavPort");
         if (caldavPort != 0) {
             SERVER_LIST.add(new CaldavServer(caldavPort));
-        }
-        int ldapPort = Settings.getIntProperty("mt.ews.ldapPort");
-        if (ldapPort != 0) {
-            SERVER_LIST.add(new LdapServer(ldapPort));
         }
 
         BundleMessage.BundleMessageList messages = new BundleMessage.BundleMessageList();
@@ -174,18 +159,6 @@ public final class MosTechEws {
         if (!errorMessages.isEmpty()) {
             MosTechEwsTray.error(new BundleMessage("LOG_MESSAGE", errorMessages));
         }
-
-        // check for new version in a separate thread
-        new Thread("CheckRelease") {
-            @Override
-            public void run() {
-                String releasedVersion = getReleasedVersion();
-                if (!currentVersion.isEmpty() && releasedVersion != null && currentVersion.compareTo(releasedVersion) < 0) {
-                    MosTechEwsTray.info(new BundleMessage("LOG_NEW_VERSION_AVAILABLE", releasedVersion));
-                }
-
-            }
-        }.start();
 
     }
 
@@ -236,23 +209,4 @@ public final class MosTechEws {
         return currentVersion;
     }
 
-    /**
-     * Get latest released version from SourceForge.
-     *
-     * @return latest version
-     */
-    public static String getReleasedVersion() {
-        String version = null;
-        if (!Settings.getBooleanProperty("mt.ews.disableUpdateCheck")) {
-            try (HttpClientAdapter httpClientAdapter = new HttpClientAdapter(HTTP_MT_EWS_SOURCEFORGE_NET_VERSION_TXT)) {
-                GetRequest getRequest = new GetRequest(HTTP_MT_EWS_SOURCEFORGE_NET_VERSION_TXT);
-                getRequest = httpClientAdapter.executeFollowRedirect(getRequest);
-                version = getRequest.getResponseBodyAsString();
-                LOGGER.debug("MT-EWS released version: " + version);
-            } catch (IOException e) {
-                MosTechEwsTray.debug(new BundleMessage("LOG_UNABLE_TO_GET_RELEASED_VERSION"));
-            }
-        }
-        return version;
-    }
 }
