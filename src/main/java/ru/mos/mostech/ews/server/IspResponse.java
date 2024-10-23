5c4cc89de5ee9aebd27ddb35a83a13e26fb14574
Andrei Zaboikin <andreizaboikin@gmail.com>
2024-10-23 23:21:11 +0300

    MOSTECHOS-61 | Попытка исправить проблему с подключением

--- src/main/java/ru/mos/mostech/ews/server/IspResponse.java
+++ src/main/java/ru/mos/mostech/ews/server/IspResponse.java
@@ -48,7 +48,8 @@ public class IspResponse {
 
 	public String build(String domain, String username, int imapPort, int smtpPort, int davPort, boolean isSecure) {
 
-		String davUrl = (isSecure ? "https://" : "http://") + "localhost:" + davPort;
+		// String davUrl = (isSecure ? "https://" : "http://") + "localhost:" + davPort;
+		String davUrl = "http://" + "localhost:" + davPort;
 		String socketType = isSecure ? "ssl" : "plain";
 		return TEMPLATE.replace("${domain}", domain)
 			.replace("${username}", username)
